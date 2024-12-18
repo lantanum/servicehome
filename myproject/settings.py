2a9e5dcb66dd68732ef969957e9c4021d6710b21
lantanum <119154526+lantanum@users.noreply.github.com>
2024-12-18 22:36:33 +0500

    db

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,19 +76,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'newservice',
-        'USER': 'root',
-        'PASSWORD': '26111304',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'use_unicode': True,
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
