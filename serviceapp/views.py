376582a64e96a49d36f1dac50fdceec50cec3c2d
lantanum <119154526+lantanum@users.noreply.github.com>
2025-05-01 23:33:32 +0500

    fixes

--- serviceapp/views.py
+++ serviceapp/views.py
@@ -24,7 +24,6 @@ from django.utils.decorators import method_decorator
 from serviceapp.amocrm_client import AmoCRMClient
 from serviceapp.deposits import apply_first_deposit_bonus, apply_registration_bonus
 from serviceapp.amocrm_client import AmoCRMClient
-from serviceapp.signals import recalc_client_balance, recalc_master_balance
 from serviceapp.utils import STATUS_MAPPING, parse_nested_form_data, MASTER_LEVEL_MAPPING
 from .serializers import (
     AmoCRMWebhookSerializer,
